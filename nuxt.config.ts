a0b4a08f33eed4872705352df6bb1345842f36b6
rosahoffer <rosahoffer@live.nl>
2023-12-18 19:01:20 +0100

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,21 +5,10 @@ export default {
   alias: {
     '@': resolve(__dirname, "/"),
   },
-  css: [
-    "@/static/styles/global.css"
-  ],
-
-  vite: {
-    build: {
-      ssr: true,
-      rollupOptions: {
-        // Verwijder de externe verwijzing naar het CSS-bestand
-        external: [],
-      },
-    },
-  },
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+
+
 };
